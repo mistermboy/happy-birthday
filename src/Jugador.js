383f9229cefbfd5b563986cc98f8cdc2d7543f9f
Pablo Menéndez Suárez ◔_◔ <pabloyo97@hotmail.com>
2018-12-30 17:13:54 +0100

    se mueve

--- src/Jugador.js
+++ src/Jugador.js
@@ -1,3 +1,4 @@
+
 var estadoQuieto = 1;
 var estadoCaminando = 2;
 
@@ -9,62 +10,53 @@ var Jugador = cc.Class.extend({
     sprite:null,
     shape:null,
     body:null,
-    tor:function (gameLayer, posicion) {
-        this.gameLayer = gameLayer;
+ctor:function (gameLayer, posicion) {
+    this.gameLayer = gameLayer;
 
-        /*
-        var framesAnimacion = [];
-        for (var i = 1; i <= 4; i++) {
-            var str = "jugador_avanzando" + i + ".png";
-            var frame = cc.spriteFrameCache.getSpriteFrame(str);
-            framesAnimacion.push(frame);
-        }
-        var animacion = new cc.Animation(framesAnimacion, 0.2);
-        var actionAnimacionBucle =
-            new cc.RepeatForever(new cc.Animate(animacion));
-        this.aCaminar = actionAnimacionBucle;
-        this.aCaminar.retain();
+    var framesAnimacion = [];
+    for (var i = 1; i <= 4; i++) {
+        var str = "jugador_avanzando" + i + ".png";
+        var frame = cc.spriteFrameCache.getSpriteFrame(str);
+        framesAnimacion.push(frame);
+    }
+    var animacion = new cc.Animation(framesAnimacion, 0.2);
+    var actionAnimacionBucle =
+        new cc.RepeatForever(new cc.Animate(animacion));
+    this.aCaminar = actionAnimacionBucle;
+    this.aCaminar.retain();
 
+    // Crear Sprite - Cuerpo y forma
+    this.sprite = new cc.PhysicsSprite("#jugador_avanzando1.png");
+    // Cuerpo dinámico, SI le afectan las fuerzas
+    this.body = new cp.Body(5, cp.momentForBox(1,
+        this.sprite.getContentSize().width,
+        this.sprite.getContentSize().height));
 
-        // Crear Sprite - Cuerpo y forma
-        this.sprite = new cc.PhysicsSprite("#jugador_avanzando1.png");
-        // Cuerpo dinámico, SI le afectan las fuerzas
-        this.body = new cp.Body(5, cp.momentForBox(1,
-            this.sprite.getContentSize().width,
-            this.sprite.getContentSize().height));
+    this.body.setPos(posicion);
+    //body.w_limit = 0.02;
+    this.body.setAngle(0);
+    this.sprite.setBody(this.body);
 
-
-
-
-
-        this.body.setPos(posicion);
-        //body.w_limit = 0.02;
-        this.body.setAngle(0);
-        this.sprite.setBody(this.body);
-
-        // Se añade el cuerpo al espacio
-        gameLayer.space.addBody(this.body);
+    // Se añade el cuerpo al espacio
+    gameLayer.space.addBody(this.body);
 
 
+    // forma 16px más pequeña que la imagen original
+    this.shape = new cp.BoxShape(this.body,
+        this.sprite.getContentSize().width - 16,
+        this.sprite.getContentSize().height - 16);
+    this.shape.setCollisionType(tipoJugador);
+    // forma dinamica
+    gameLayer.space.addShape(this.shape);
 
 
-        // forma 16px más pequeña que la imagen original
-        this.shape = new cp.BoxShape(this.body,
-            this.sprite.getContentSize().width - 16,
-            this.sprite.getContentSize().height - 16);
-        this.shape.setCollisionType(tipoJugador);
-        // forma dinamica
-        gameLayer.space.addShape(this.shape);
+    // ejecutar la animación
+    this.sprite.runAction(actionAnimacionBucle);
+    // añadir sprite a la capa
+    gameLayer.addChild(this.sprite,10);
 
 
-
-        // ejecutar la animación
-        this.sprite.runAction(actionAnimacionBucle);
-        // añadir sprite a la capa
-        gameLayer.addChild(this.sprite,10);
-
-       */
-    },
+},
     actualizar: function (){
         switch ( this.estado ){
             case estadoCaminando:
@@ -75,7 +67,8 @@ var Jugador = cc.Class.extend({
                 }
                 break;
         }
-    }
+}
+
 
 
 });
--- src/resource.js
+++ src/resource.js
@@ -2,13 +2,26 @@ var res = {
     HelloWorld_png : "res/HelloWorld.png",
     CloseNormal_png : "res/CloseNormal.png",
     CloseSelected_png : "res/CloseSelected.png",
-    bola_png : "res/bola.png",
-    animacionbola_png : "res/animacionbola.png",
-    animacionbola_plist:"res/animacionbola.plist",
-    fondo_png : "res/fondo.png",
-    cocodrilo_1_png : "res/cocodrilo_1.png",
-    animacioncocodrilo_png : "res/animacioncocodrilo.png",
-    animacioncocodrilo_plist : "res/animacioncocodrilo.plist"
+    boton_jugar_png : "res/boton_jugar.png",
+    menu_titulo_png : "res/menu_titulo.png",
+    tiles32_png: "res/tiles32.png",
+    mapa1_tmx: "res/mapa1.tmx",
+    moneda_png : "res/moneda.png",
+    moneda_plist : "res/moneda.plist",
+    jugador_avanzando_png : "res/jugador_avanzando.png",
+    jugador_avanzando_plist : "res/jugador_avanzando.plist",
+    jugador_caminar_png : "res/jugador_caminar.png",
+    jugador_caminar_plist : "res/jugador_caminar.plist",
+    jugador_subiendo_png : "res/jugador_subiendo.png",
+    jugador_subiendo_plist : "res/jugador_subiendo.plist",
+    jugador_impactado_png: "res/jugador_impactado.png",
+    jugador_impactado_plist: "res/jugador_impactado.plist",
+    boton_saltar_png: "res/boton_saltar.png",
+    animacion_cuervo_png: "res/animacion_cuervo.png",
+    animacion_cuervo_plist: "res/animacion_cuervo.plist",
+    animaciontigre_plist : "res/animaciontigre.plist",
+    animaciontigre_png: "res/animaciontigre.png",
+    box_red_png : "res/box-red.png"
 };
 
 var g_resources = [];
